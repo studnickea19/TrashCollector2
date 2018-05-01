159c22519aed212695f50bab604a4a1f02d72ad6
Ellie Studnicka <e.studnicka@gmail.com>
2018-05-01 08:05:58 -0500

    FInally got Foreign Keys working, problems caused by many to many relationship. Working on adding controllers

---
+++ TrashCollector2/TrashCollector2/Controllers/ChargesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollector2.Models;
+
+namespace TrashCollector2.Controllers
+{
+    public class ChargesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Charges
+        public ActionResult Index()
+        {
+            var charge = db.Charge.Include(c => c.Customer).Include(c => c.Pickup);
+            return View(charge.ToList());
+        }
+
+        // GET: Charges/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Charge charge = db.Charge.Find(id);
+            if (charge == null)
+            {
+                return HttpNotFound();
+            }
+            return View(charge);
+        }
+
+        // GET: Charges/Create
+        public ActionResult Create()
+        {
+            ViewBag.CustomerID = new SelectList(db.Customer, "CustomerID", "FirstName");
+            ViewBag.PickupID = new SelectList(db.Pickup, "PickupID", "PickupID");
+            return View();
+        }
+
+        // POST: Charges/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ChargeID,ChargeAmount,PickupID,CustomerID")] Charge charge)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Charge.Add(charge);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CustomerID = new SelectList(db.Customer, "CustomerID", "FirstName", charge.CustomerID);
+            ViewBag.PickupID = new SelectList(db.Pickup, "PickupID", "PickupID", charge.PickupID);
+            return View(charge);
+        }
+
+        // GET: Charges/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Charge charge = db.Charge.Find(id);
+            if (charge == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CustomerID = new SelectList(db.Customer, "CustomerID", "FirstName", charge.CustomerID);
+            ViewBag.PickupID = new SelectList(db.Pickup, "PickupID", "PickupID", charge.PickupID);
+            return View(charge);
+        }
+
+        // POST: Charges/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ChargeID,ChargeAmount,PickupID,CustomerID")] Charge charge)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(charge).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CustomerID = new SelectList(db.Customer, "CustomerID", "FirstName", charge.CustomerID);
+            ViewBag.PickupID = new SelectList(db.Pickup, "PickupID", "PickupID", charge.PickupID);
+            return View(charge);
+        }
+
+        // GET: Charges/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Charge charge = db.Charge.Find(id);
+            if (charge == null)
+            {
+                return HttpNotFound();
+            }
+            return View(charge);
+        }
+
+        // POST: Charges/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Charge charge = db.Charge.Find(id);
+            db.Charge.Remove(charge);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TrashCollector2/TrashCollector2/Models/IdentityModels.cs
@@ -0,0 +1,46 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace TrashCollector2.Models
+{
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<PickUpArea> PickUpArea { get; set; }
+        public DbSet<Address> Address { get; set; }
+        public DbSet<Pickup> Pickup { get; set; }
+        public DbSet<Charge> Charge { get; set; }
+        public DbSet<Employee> Employee { get; set; }
+
+        public DbSet<CustomerAddress> CustomerAddress { get; set; }
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Address>().HasRequired(c => c.User).WithMany().WillCascadeOnDelete(false);
+        //}
+    }
+}
